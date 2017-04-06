9136165013c964750dbfcebca204410b7c3c7daf
JoshuaWeinstein <jweinst1@berkeley.edu>
2017-04-06 16:28:46 -0700

    display, debug, e.g., ne for mushroom

--- src/mushroom.rs
+++ src/mushroom.rs
@@ -1,19 +1,52 @@
-//file for mushroom value
+use std::fmt;
+
+
 
 pub enum MushRoom {
 	Int(i32),
 	Bool(bool),
+	List(Vec<MushRoom>),
 	Func(fn(Vec<MushRoom>) -> MushRoom)
 }
 
 impl MushRoom {
-	fn plus(self, other:MushRoom) -> MushRoom {
-		match self {
-			MushRoom::Int(i) => match other {
-				MushRoom::Int(j) => MushRoom::Int(i + j),
-				_ => i + 0
-			},
-			_ => 0
+    pub fn repr(value:&MushRoom) -> String {
+		match *value {
+			MushRoom::Int(i) => format!("{}", i),
+			MushRoom::Bool(b) => format!("{}", b),
+			MushRoom::Func(f) => format!("{:?}", f),
+			MushRoom::List(ref l) => {
+				let mut fmt_str = "[ ".to_string();
+				for elem in l {
+					fmt_str += MushRoom::repr(elem).as_str();
+					fmt_str += " ";
+				}
+				fmt_str += "]";
+				fmt_str
+			}
 		}
 	}
+}
+
+
+impl fmt::Debug for MushRoom {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f, "{}", MushRoom::repr(self))
+	}
+}
+
+impl fmt::Display for MushRoom {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl PartialEq for MushRoom {
+    fn eq(&self, other: &MushRoom) -> bool {
+        MushRoom::repr(self) == MushRoom::repr(other)
+    }
+    
+    fn ne(&self, other: &MushRoom) -> bool {
+        MushRoom::repr(self) != MushRoom::repr(other)
+    }
 }
